728593650d655bafd0627be5465272892266dda5
João Guilherme <joaopessanhaalves@gmail.com>
2021-01-15 22:36:13 -0300

    added node aula2

--- node/aula1.js
+++ node/aula1.js
@@ -1,7 +1,7 @@
-var somaFunc = require("./soma");
-var subFunc = require("./sub");
-var multiFunc = require("./multi");
-var divFunc = require("./div");
+var somaFunc = require("./modulos/soma");
+var subFunc = require("./modulos/sub");
+var multiFunc = require("./modulos/multi");
+var divFunc = require("./modulos/div");
 
 console.log(somaFunc(1,2));
 console.log(subFunc(1,2));
