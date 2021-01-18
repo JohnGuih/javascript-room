7961c072fde548f5d98be0ae70ef8eabdefb4944
João Guilherme <joaopessanhaalves@gmail.com>
2021-01-18 14:29:48 -0300

    aula node update

--- node/aula3.js
+++ node/aula3.js
@@ -14,6 +14,11 @@ app.get("/blog", function(req, res){
     res.send("Bem-vindo ao meu blog")
 });
 
+//So pode aver uma .send em cada função
+app.get('/ola/:nome', function(req, res){
+    res.send("<h1>Ola " + req.params.nome + "</h1>");
+});
+
 
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081");
