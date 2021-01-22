c98f27a7f4f5083e30685299cea934a363afd917
João Guilherme <joaopessanhaalves@gmail.com>
2021-01-22 13:12:35 -0300

    project update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 const bodyParser = require("body-parser");
-const Sequelize = require('sequelize');
+const Post = require("./models/Post")
 
 
 
@@ -13,19 +13,36 @@ const Sequelize = require('sequelize');
     // Bpdy Parser
         app.use(bodyParser.urlencoded({extended: false}));
         app.use(bodyParser.json());
-    // Conexão com o banco de dados
-        const sequelize = new Sequelize('teste', 'root', 'root', {
-            host: "localhost",
-            dialect: 'mysql'
-        });
+    
 // Rotas
-        app.get("/cad", function(req, res){
-            res.render("formulario");
-        });
+    app.get("/", function(req, res){
+        Post.findAll({order: [['id', 'DESC']]}).then(function(posts){
+            res.render("home", {posts: posts});
+        })
+    });
 
-        app.post("/add", function(req, res){
-            res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo);
+    app.get("/cad", function(req, res){
+        res.render("formulario");
+    });
+
+    app.post("/add", function(req, res){
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect("/")
+        }).catch(function(erro){
+            res.send("Houve um erro: "+erro)
         });
+    });
+
+    app.get('/deletar/:id', function(req, res){
+        Post.destroy({where: {'id': req.params.id}}).then(function(){
+            res.send("Postagem deletada com sucesso!")
+        }).catch(function(erro){
+            res.send("Esta postagem não existe!")
+        })
+    })
 
 app.listen(8081, function(){
     console.log("Servidor rodando na url http://localhost:8081");
