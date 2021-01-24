497df220ab487fd6580f931de20b45a93b9846b3
João Guilherme <joaopessanhaalves@gmail.com>
2021-01-24 16:25:56 -0300

    project update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
+const path = require('path')
 //const mongoose = require('mongoose')
 
 // Configurações
@@ -14,6 +15,9 @@ const admin = require('./routes/admin')
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
     // Mongoose
+        // Em breve
+    // Public
+        app.use(express.static(path.join(__dirname, "public")))
 
 // Rotas
     app.get('/', (req, res)=>{
