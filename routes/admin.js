e2ce0a60f5452abc12a170e041bde9ba3d3a0360
João Guilherme <joaopessanhaalves@gmail.com>
2021-01-31 17:54:30 -0300

    added deletar postagem

--- routes/admin.js
+++ routes/admin.js
@@ -239,4 +239,14 @@ router.post("/postagem/edit", (req, res)=>{
     }
 })
 
+router.get("/postagens/deletar/:id", (req, res)=>{
+    Postagem.findOneAndDelete({_id: req.params.id}).then(()=>{
+        req.flash("success_msg", "Deletado com sucesso")
+        res.redirect("/admin/postagens")
+    }).catch((err)=>{
+        req.flash("error_msg", "Houve um erro")
+        res.redirect("/admin/postagens")
+    })
+})
+
 module.exports = router
