726633a415dd02e75c1964c297993c09e162c97d
João Guilherme <joaopessanhaalves@gmail.com>
2021-02-01 19:32:44 -0300

    added index page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require('path')
 const mongoose = require('mongoose')
 const session = require("express-session")
 const flash = require("connect-flash")
+require("./models/Postagem")
+const Postagem = mongoose.model("postagens")
 
 // Configurações
     // Sessão
@@ -41,8 +43,22 @@ const flash = require("connect-flash")
 
 // Rotas
     app.get('/', (req, res)=>{
-        res.send('rota principal')
+        Postagem.find().lean().populate("categoria").sort({data:"desc"}).then((postagens)=>{
+            res.render("index", {postagens: postagens})
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um error interno")
+            res.redirect("/404")
+        })
     })
+    
+    app.get("/404", (req, res)=>{
+        res.send('Erro 404!')
+    })
+
+    app.get("/posts", (req, res)=>{
+        res.send("Lista Posts")
+    })
+
     app.use('/admin', admin)
 // Outros
 const PORT = 8081
