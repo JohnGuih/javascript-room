2e046b3003a56b595bac0648dd30f426ba83e061
João Guilherme <joaopessanhaalves@gmail.com>
2021-02-02 15:16:54 -0300

    finished user interface

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const session = require("express-session")
 const flash = require("connect-flash")
 require("./models/Postagem")
 const Postagem = mongoose.model("postagens")
+require("./models/Categoria")
+const Categoria = mongoose.model("categorias")
 
 // Configurações
     // Sessão
@@ -48,6 +50,34 @@ const Postagem = mongoose.model("postagens")
         }).catch((err)=>{
             req.flash("error_msg", "Houve um error interno")
             res.redirect("/404")
+        })
+    })
+
+    app.get("/categorias", (req, res)=>{
+        Categoria.find().lean().then((categorias)=>{
+            res.render("categorias/index", {categorias: categorias})
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/categorias/:slug", (req, res)=>{
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria)=>{
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).lean().then((postagens)=>{
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria})
+                }).catch((err)=>{
+                    req.flash("error_msg", "Houve um erro ao listar os posts!")
+                    res.redirect("/categorias")
+                })
+            }else{
+                req.flash("error_msg", "Esta categoria não existe")
+                res.redirect("/categorias")
+            }
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro ao carregar a pagina")
+            res.redirect("/categorias")
         })
     })
 
