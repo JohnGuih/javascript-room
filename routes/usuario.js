5d44e8338cbdb6e4f89e758814a1a49a6a3272a2
João Guilherme <joaopessanhaalves@gmail.com>
2021-02-12 16:07:49 -0300

    added login authenticator

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
+const bcrypt = require("bcryptjs")
+const passport = require("passport")
 
 router.get("/registro", (req, res)=>{
     res.render("usuarios/registro")
@@ -34,8 +36,53 @@ router.post("/registro", (req, res)=>{
     if(erros.length > 0){
         res.render("usuarios/registro", {erros: erros})
     }else{
+        Usuario.findOne({email:req.body.email}).lean().then((usuario)=>{
+            if(usuario){
+                req.flash("error_msg", "Já existe uma conta com este email")
+                res.redirect("/usuarios/registro")
+            }else{
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
 
+                bcrypt.genSalt(10, (erro, salt)=>{
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash)=>{
+                        if(erro){
+                            req.flash("error_msg", "Houve um erro durante o salvamento do usuario")
+                            res.redirect("/usuarios/registro")
+                        }
+
+                        novoUsuario.senha = hash
+
+                        novoUsuario.save().then(()=>{
+                            req.flash("success_msg", "Usuario salvo com sucesso")
+                            res.redirect("/")
+                        }).catch((err)=>{
+                            req.flash("error_msg", "Houve um erro ao salvar o usuario")
+                            res.redirect("/usuarios/registro")
+                        })
+                    })
+                })
+            }
+        }).catch((err)=>{
+            req.flash("error-msg", "Houve um erro interno")
+            res.redirect("/registro")
+        })
     }
+})
+
+router.get("/login", (req, res)=>{
+    res.render("usuarios/login")
+})
+
+router.post("/login", (req, res, next)=>{
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/usuarios/login",
+        failureFlash: true
+    })(req, res, next)
 })
 
 module.exports = router
